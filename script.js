c9338b49761eb0e07f158f59a446a43679717541
Roni Wellington <roniwellington@gmail.com>
2022-12-16 21:32:13 -0300

    projeto concluido

--- script.js
+++ script.js
@@ -29,12 +29,15 @@ addEventOnElements(navTogglers, "click", toggleNav);
 */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", () => {
   if(window.scrollY > 100){
     header.classList.add("active")
+    backTopBtn.classList.add("active")
   } else {
     header.classList.remove("active")
+    backTopBtn.classList.remove("active")
   }
 })
 
